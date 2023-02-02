c75431a21c59d2daa2bf9290300e65d27a84a4c9
BitByte <adilrahman20008@gmail.com>
2023-02-02 13:50:58 +0530

    remove digit from valid checking

--- yake/datarepresentation.py
+++ yake/datarepresentation.py
@@ -204,7 +204,7 @@ class composed_word(object):
     def isValid(self):
         isValid = False
         for tag in self.tags:
-            isValid = isValid or ( "u" not in tag and "d" not in tag )
+            isValid = isValid or ( "u" not in tag)
         return isValid and not self.start_or_end_stopwords
 
     def get_composed_feature(self, feature_name, discart_stopword=True):
